3bc96f6e35207b92fe667b8dc2a575ca5351a38e
Elves Trindade <elvestrindade31@gmail.com>
2019-11-24 21:38:30 +0100

    Full github app

---
+++ src/pages/User/index.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
+
+import '../../config/ReactotronConfig';
+import api from '../../services/api';
+
+// import { Container } from './styles';
+
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+
+    const user = navigation.getParam('user');
+
+    const responde = await api.get(`users/${user.login}/starred`);
+
+    this.setState({ stars: responde.data });
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const { stars } = this.state;
+
+    const user = navigation.getParam('user');
+    console.tron.log(user);
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
+  }
+}
